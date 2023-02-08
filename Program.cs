600f0be4870d9c1f781ff7d7a3563b97e5a27192
Aldoshin_Alexandr <aldoshin_a@internet.ru>
2023-02-08 16:38:07 +0300

    Решение задачи № 50 Семинар № 7, программа находит и выводит значение ячейки по указанным координатам + ищет указанное значение, адрес расположения всех совпадений

--- Program.cs
+++ Program.cs
@@ -9,39 +9,58 @@
 
 // 17 -> такого числа в массиве нет
 
-Console.Write("Enter value = nums Rows : ");
+Console.Write("Enter value = number of Rows : ");
 int x = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter value = nums Columns : ");
+Console.Write("Enter value = number of Columns : ");
 int y = Convert.ToInt32(Console.ReadLine());
 
-Console.Write("Enter # Rows : ");
+Console.Write("Enter the number of the row in which the cell is located : ");
 int m = Convert.ToInt32(Console.ReadLine());
-Console.Write("Enter # Columns : ");
+Console.Write("Enter the number of the column in which the cell is located : ");
 int n = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Enter the value you want to find : ");
+int SRCValue = Convert.ToInt32(Console.ReadLine());
+
 int[,] Matrix = new int[x, y];
-int Result = 0;
+int count = 0;
 
 void SRCPosition(int[,] Matrix, int m, int n)
 {
-    bool flag;
+    int Result = 0;
+    bool Flag;
     for (int i = 0; i < x; i++)
     {
         for (int j = 0; j < y; j++)
         {
             Matrix[i, j] = new Random().Next(0, 10);
-            if (i == m && j == n) { Result = Matrix[i, j]; flag = true; }
+            if (i == m && j == n) { Result = Matrix[i, j]; Flag = true; }
+            if (Matrix[i, j] == SRCValue)
+            {
+                count++;
+            }
             Console.Write($"{Matrix[i, j]} ");
         }
         Console.WriteLine();
     }
-    if (flag = true)
+    if (Flag = true) { Console.WriteLine(Result); }
+    else { Console.WriteLine("This cell does not exist in the array"); }
+}
+
+void SRCValuePosition(int[,] Matrix)
+{
+    for (int i = 0; i < x; i++)
     {
-        Console.WriteLine(Result);
-    }
-    else
-    {
-        Console.WriteLine("Данной ячейки не существует");
+        for (int j = 0; j < y; j++)
+        {
+            if (Matrix[i, j] == SRCValue)
+            { Console.WriteLine($"{Matrix[i, j]} --> ({i}, {j})"); }
+            else if (i == x -1 && j == y - 1 && Matrix[i, j] != SRCValue)
+            {
+                Console.WriteLine("This value is not in the array");
+            }
+        }
     }
 }
 SRCPosition(Matrix, m, n);
+SRCValuePosition(Matrix);
